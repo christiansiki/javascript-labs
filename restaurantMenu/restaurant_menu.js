4d806ede2ca00dfa0e020657fa228a64f25ad021
christiansiki <christiansiki94@live.com>
2025-02-23 19:35:29 -0500

    added prices

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,6 +1,6 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes - $12', 'Eggs Benedict - $22.99', 'Oatmeal - $21.99', 'Frittata - $15'];
+const mainCourseMenu = ['Steak - $32.50', 'Pasta - $25', 'Burger - $22.99', 'Salmon - $24'];
+const dessertMenu = ['Cake - $7.99', 'Ice Cream - 6.99', 'Pudding - 7.50', 'Fruit Salad - $8.50'];
 
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index +1}: ${item}</p>`).join('');
 document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
